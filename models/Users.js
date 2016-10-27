29c79060d031b7b3568177e16fc0d21ca7eda431
Yash Kapila <yash.kapila18@gmail.com>
2016-10-27 13:34:24 +0530

    Create Users.js

---
+++ models/Users.js
@@ -0,0 +1,46 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        // validator: function(){
+            
+        // }
+        required: true
+    }
+});
+
+// add methods to schema. This is done at 'repository' layer in Spring MVC.
+UserSchema.methods.verifyUsername = function(callback) {
+    return this.model('User').findOne({ username: this.username }, callback);
+};
+
+UserSchema.methods.verifyPassword = function(callback) {
+    return this.model('User').findOne(
+        { 
+            $and: [
+                { username: this.username },
+                { password: this.password }
+            ]
+        }, callback);
+};
+
+UserSchema.methods.addUser = function(callback) {
+    return this.model('User').create(
+        { 
+            username: this.username, 
+            password: this.password, 
+            email: this.email 
+        }, callback);
+};
+
+exports = module.exports = mongoose.model('User', UserSchema);
